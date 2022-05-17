90882cca0ddb036b67ef7ea19657a67193477887
hay-kal <21040411@myrp.edu.sg>
2022-05-17 12:20:52 +0800

    k

--- app/src/main/java/sg/edu/rp/c346/id21040411/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21040411/democheckboxexample/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Button Click", Toast.LENGTH_LONG).show();
                 if (!cbEnabled.isChecked()) {
                     tvShow.setText("The discount is not given.");
                 } else {
